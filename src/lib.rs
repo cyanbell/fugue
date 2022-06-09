b5ffe849d15ccf81fe71b9bd7083410f6887f0db
shengurun <shengurun@gmail.com>
2022-06-09 21:46:53 +0800

    feat: add new function to convert markdown file
    1. read file content to `String` by `fs::read_to_string`.
    2. use `convert` function to convert file content.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+use std::fs;
 
 use pyo3::prelude::*;
 use pulldown_cmark::{Parser, html};
@@ -13,9 +14,18 @@ fn convert(text: &str) -> PyResult<String> {
     Ok(output)
 }
 
+// Convert Markdown file to HTML.
+#[pyfunction]
+fn convert_from_file(filename: &str) -> PyResult<String> {
+    let content = fs::read_to_string(filename)?;
+
+    convert(&content)
+}
+
 /// A Python module implemented in Rust.
 #[pymodule]
 fn fugue(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(convert, m)?)?;
+    m.add_function(wrap_pyfunction!(convert_from_file, m)?)?;
     Ok(())
 }
