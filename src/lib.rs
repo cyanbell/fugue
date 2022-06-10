337f37b4c9c2ca6966b4bc550441c210cdbeb42a
shengurun <shengurun@gmail.com>
2022-06-10 22:36:21 +0800

    feat: add class `Markdown`
    1. create class `Markdown`, and add methods.
    2. use macro `lazy_static!` declare lazily evaluated static `HashMap`.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,85 @@
 use std::fs;
+use std::collections::HashMap;
 
 use pyo3::prelude::*;
-use pulldown_cmark::{Parser, html};
+use pulldown_cmark::{Parser, html, Options};
 
-// Convert Markdown to HTML.
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static!(
+    static ref PLUGINS: HashMap<&'static str, Options> = HashMap::from([
+        ("tables", Options::ENABLE_TABLES),
+        ("footnotes", Options::ENABLE_FOOTNOTES),
+        ("strikethrough", Options::ENABLE_STRIKETHROUGH),
+        ("tasklists", Options::ENABLE_TASKLISTS),
+        ("smart_punctuation", Options::ENABLE_SMART_PUNCTUATION),
+        ("heading_attributes", Options::ENABLE_HEADING_ATTRIBUTES)
+    ]);
+);
+
+#[pyclass]
+struct Markdown {
+    options: Options
+}
+
+#[pymethods]
+impl Markdown {
+    // Generate `Markdown` with any plugins or without plugin.
+    #[new]
+    fn new(plugins: Option<Vec<&str>>) -> Self {
+        if let Some(plugins) = plugins {
+            let mut options = Options::empty();
+
+            // Iterate over plugins and take corresponding `Option`
+            for plugin_name in plugins.iter() {
+                if let Some(option) = PLUGINS.get(plugin_name) {
+                    options.insert(*option)
+                }
+            }
+
+            return Self {
+                options: options
+            }
+        } else {
+            return Self {
+                options: Options::empty()
+            }
+        }
+    }
+
+    // Convert Markdown to HTML.
+    fn convert(&self, text: &str) -> PyResult<String> {
+        let parser = Parser::new_ext(text, self.options);
+
+        let mut output = String::new();
+        html::push_html(&mut output, parser);
+    
+        Ok(output)
+    }
+
+    // Convert Markdown file to HTML.
+    fn convert_from_file(&self, filename: &str) -> PyResult<String> {
+        let content = fs::read_to_string(filename)?;
+
+        self.convert(&content)
+    }
+
+    fn __call__(&self, text: &str) -> PyResult<String> {
+        self.convert(text)
+    }
+}
+
+// Create `Markdown` instance, the same as `Markdown()`.
+#[pyfunction]
+fn create_markdown(plugins: Option<Vec<&str>>) -> PyResult<Markdown> {
+    let markdown = Markdown::new(plugins);
+
+    Ok(markdown)
+}
+
+
+// Convert Markdown to HTML(without plugin).
 #[pyfunction]
 fn convert(text: &str) -> PyResult<String> {
     let parser = Parser::new(text);
@@ -14,7 +90,7 @@ fn convert(text: &str) -> PyResult<String> {
     Ok(output)
 }
 
-// Convert Markdown file to HTML.
+// Convert Markdown file to HTML(without plugin).
 #[pyfunction]
 fn convert_from_file(filename: &str) -> PyResult<String> {
     let content = fs::read_to_string(filename)?;
@@ -27,5 +103,7 @@ fn convert_from_file(filename: &str) -> PyResult<String> {
 fn fugue(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(convert, m)?)?;
     m.add_function(wrap_pyfunction!(convert_from_file, m)?)?;
+    m.add_function(wrap_pyfunction!(create_markdown, m)?)?;
+    m.add_class::<Markdown>()?;
     Ok(())
 }
