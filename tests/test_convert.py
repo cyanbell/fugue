27ccac31af1aefecb02331160ea08755b13b3f8c
shengurun <shengurun@gmail.com>
2022-06-12 20:14:39 +0800

    test: add markdown syntax examples
    1. add markdown syntax examples in markdown text.
    2. change html content, make sure test cases passed.

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -8,18 +8,78 @@ class ConvertTestCase(TestCase):
     def setUp(self) -> None:
         self.text = """# Title
 
-## Second title
+> This is a blockquote.
 
-```python
-print("Hello, world")
-```
-"""
+## Ordered list
+
+1. **First**
+2. *Second*
+3. Third
+
+## Code
+
+- Code block:
+
+    ```python
+    def main() -> None:
+        print("Hello, world.")
+    ```
+
+- Span of code:
+
+    `print(Hello, world.)`
+
+## Links and Image
+
+### Links
+
+Go to [Github](https://github.com).  
+Go to [Google][Google].
+
+[Google]: https://www.google.com
+
+---
+
+### Image
+
+Rust logo:
+
+![Rust](https://www.rust-lang.org/static/images/rust-logo-blk.svg)
+
+""" # noqa
 
         self.html = """<h1>Title</h1>
-<h2>Second title</h2>
-<pre><code class="language-python">print(&quot;Hello, world&quot;)
+<blockquote>
+<p>This is a blockquote.</p>
+</blockquote>
+<h2>Ordered list</h2>
+<ol>
+<li><strong>First</strong></li>
+<li><em>Second</em></li>
+<li>Third</li>
+</ol>
+<h2>Code</h2>
+<ul>
+<li>
+<p>Code block:</p>
+<pre><code class="language-python">def main() -&gt; None:
+    print(&quot;Hello, world.&quot;)
 </code></pre>
-"""
+</li>
+<li>
+<p>Span of code:</p>
+<p><code>print(Hello, world.)</code></p>
+</li>
+</ul>
+<h2>Links and Image</h2>
+<h3>Links</h3>
+<p>Go to <a href="https://github.com">Github</a>.<br />
+Go to <a href="https://www.google.com">Google</a>.</p>
+<hr />
+<h3>Image</h3>
+<p>Rust logo:</p>
+<p><img src="https://www.rust-lang.org/static/images/rust-logo-blk.svg" alt="Rust" /></p>
+""" # noqa
 
         return super().setUp()
 
